930271db29c704cdfc11a88b015ab61225d12be9
Victor Tulnikov <Tulnikov9@mail.ru>
2023-01-02 13:54:13 +0300

    Задание 4

---
+++ Seminar 1/HomeWork 4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Данная программа выводит на экран все четные числа от 1 до заданного числа
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+for(int i =1 ; i <= n; i++)
+    if(i%2==0)
+       Console.WriteLine(i);
