cc3d7477224b5b68d8023eccbb94784f5e9c647d
Victor Tulnikov <Tulnikov9@mail.ru>
2023-01-02 13:17:46 +0300

    Задание 3

---
+++ Seminar 1/homeWork 3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Данная программа определит является ли число четным");
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a%2==1)
+{
+    Console.WriteLine("Число нечетное");
+}
+else 
+{
+    Console.WriteLine("Число четное");
+}
