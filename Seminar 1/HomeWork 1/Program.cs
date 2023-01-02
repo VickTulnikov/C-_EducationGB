05681bedc8b45893229bac1b1d8c0d58a161a703
Victor Tulnikov <Tulnikov9@mail.ru>
2023-01-02 12:42:23 +0300

    Задание 1

--- Seminar 1/HomeWork 1/Program.cs
+++ Seminar 1/HomeWork 1/Program.cs
@@ -5,10 +5,19 @@ Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
 if(number1>number2)
-    Console.WriteLine($"Большее = {number1}"); 
+{
+    Console.WriteLine($"Большее = {number1}");
+    Console.WriteLine($"Меньшее = {number2}"); 
+ }
+    
 
 if(number2>number1)
+{
     Console.WriteLine($"Большее = {number2}");
-
+    Console.WriteLine($"Меньшее = {number1}");
+}
+    
 if(number1==number2)
-     Console.WriteLine("Числа равны");
+{
+Console.WriteLine("Числа равны");
+} 
