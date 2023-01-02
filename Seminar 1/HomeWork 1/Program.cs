0d36d36cbdb23d8bfe88d32627f41fe293e01ab7
Victor Tulnikov <Tulnikov9@mail.ru>
2023-01-02 12:36:02 +0300

    Задание 1

---
+++ Seminar 1/HomeWork 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1>number2)
+    Console.WriteLine($"Большее = {number1}"); 
+
+if(number2>number1)
+    Console.WriteLine($"Большее = {number2}");
+
+if(number1==number2)
+     Console.WriteLine("Числа равны");
