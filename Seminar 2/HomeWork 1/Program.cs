85ca772c445d8913d7baacab0f27b03feb6df4e8
Victor Tulnikov <Tulnikov9@mail.ru>
2023-01-03 16:21:54 +0300

    Семинар 2 Задание 1

--- Seminar 2/HomeWork 1/Program.cs
+++ Seminar 2/HomeWork 1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите трёхзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstNumber = number / 10;
+int secondNumber = firstNumber % 10;
+
+System.Console.WriteLine(secondNumber);
